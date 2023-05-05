470490949bd54055850e8457670084267f6bcc56
tarari <t.jimenez@escolesnuria.cat>
2023-05-05 20:25:33 +0200

    Token to responses

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
 
 class DashboardController extends Controller
 {
 
-    function index(){
+    function index(Request $request){
         $url=env('APP_URL').'/api/posts';
-        $response=Http::get($url);
+        $bearer= Auth::user()->createToken('API_TOKEN')->plainTextToken;
+        $response=Http::withToken($bearer)->get($url);
         $data = json_decode($response->body(), true);
         return view('dashboard',['posts'=>$data]);
 
