4d110b8bfc9eb9cc8c8655505d7d357ec396703b
tarari <t.jimenez@escolesnuria.cat>
2023-05-05 19:00:27 +0200

    post seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +19,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $user=User::find(1);
+        $user->posts()->create(['message'=>'New message3']);
     }
 }
